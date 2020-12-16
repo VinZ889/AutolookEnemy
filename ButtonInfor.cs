69f8f4bfe4ba1506365116de65de5c4ae05c9119
VinZ889 <68568640+VinZ889@users.noreply.github.com>
2020-12-16 08:51:22 +0800

    Buy and sell items
    Using this script, you are able to sell and buy items

---
+++ ButtonInfor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonInfor : MonoBehaviour
+{
+    public int ItemID;
+    public Text PriceTxt;
+    public Text QuantityTxt;
+    public GameObject shopManager;
+
+    void Update()
+    {
+        PriceTxt.text = "Price : $ " + shopManager.GetComponent<ShopManager>().shopItems[2, ItemID].ToString();
+        QuantityTxt.text = shopManager.GetComponent<ShopManager>().shopItems[3, ItemID].ToString();
+    }
+}
---
+++ ShopManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ShopManager : MonoBehaviour
+{
+    public int[,] shopItems = new int[5, 5];
+    public float coins;
+    public Text CoinsTxt;
+    
+    void Start()
+    {
+        CoinsTxt.text = "Coins :" + coins.ToString();
+
+        //ID
+        shopItems[1, 1] = 1;
+        shopItems[1, 2] = 2;
+        shopItems[1, 3] = 3;
+        shopItems[1, 4] = 4;
+        
+
+        //Price
+        shopItems[2, 1] = 50;
+        shopItems[2, 2] = 100;
+        shopItems[2, 3] = 150;
+        shopItems[2, 4] = 400;
+        
+
+        //Qty
+        shopItems[3, 1] = 0;
+        shopItems[3, 2] = 0;
+        shopItems[3, 3] = 0;
+        shopItems[3, 4] = 0;
+        
+    }
+
+    
+    public void Buy()
+    {
+        GameObject ButtonRef = GameObject.FindGameObjectWithTag("Event").GetComponent<EventSystem>().currentSelectedGameObject;
+
+        if (coins >= shopItems[2, ButtonRef.GetComponent<ButtonInfor>().ItemID])
+        {
+            coins -= shopItems[2, ButtonRef.GetComponent<ButtonInfor>().ItemID];
+            shopItems[3, ButtonRef.GetComponent<ButtonInfor>().ItemID]++;
+            CoinsTxt.text = " Coins : " + coins.ToString();
+            ButtonRef.GetComponent<ButtonInfor>().QuantityTxt.text = shopItems[3, ButtonRef.GetComponent<ButtonInfor>().ItemID].ToString();
+        }
+    }
+
+    public void Sell()
+    {
+        GameObject ButtonRef2 = GameObject.FindGameObjectWithTag("Event").GetComponent<EventSystem>().currentSelectedGameObject;
+
+        coins += shopItems[2, ButtonRef2.GetComponent<ButtonInfor>().ItemID];
+        shopItems[3, ButtonRef2.GetComponent<ButtonInfor>().ItemID]--;
+        CoinsTxt.text = " Coins : " + coins.ToString();
+        ButtonRef2.GetComponent<ButtonInfor>().QuantityTxt.text = shopItems[3, ButtonRef2.GetComponent<ButtonInfor>().ItemID].ToString();
+        
+    }
+}
